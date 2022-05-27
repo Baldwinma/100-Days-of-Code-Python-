2cba879330727e17736e31cc287cb4b3c1c3f0ce
Baldwin Mbangwana <baldwinapiafor@gmail.com>
2022-05-27 02:09:14 -0400

    final commit

--- AWS_Projects/sourcefiles/aws_ec2_create_instance.py
+++ AWS_Projects/sourcefiles/aws_ec2_create_instance.py
@@ -6,31 +6,79 @@
 import boto3
 
 def create_ec2_instance():
+    """
+    MaxCount=1, # Keep the max count to 1, unless you have a requirement to increase it
+    InstanceType="t2.micro", # Change it as per your need, But use the Free tier one
+    KeyName="ec2-key" # Change it to the name of the key you have.
+    :return: Creates the EC2 instance.
+    """
     try:
-        print("Creating EC2 Instance....")
-
+        print ("Creating EC2 instance")
         resource_ec2 = boto3.client("ec2")
         resource_ec2.run_instances(
-        ImageId = "ami-0022f774911c1d690",
-        MinCount = 1,
-        MaxCount = 1,
-        InstanceType = "t2.micro",
-        KeyName="main-key"
-    )
+            ImageId="ami-0022f774911c1d690",
+            MinCount=1,
+            MaxCount=1,
+            InstanceType="t2.micro",
+            KeyName="main-key"
+        )
     except Exception as e:
-        print("e")
+        print(e)
 
-#Describing EC2 instances
+
 def describe_ec2_instance():
     try:
-        print("Describing EC2 Instance....")
+        print ("Describing EC2 instance")
         resource_ec2 = boto3.client("ec2")
-        print(resource_ec2.describe_instances())
-        #return str(resource_ec2.describe_instances())
-        #print(resource_ec2.describe_instaces)["InstaceId"]
-    
+        print(resource_ec2.describe_instances()["Reservations"][0]["Instances"][0]["InstanceId"])
+        return str(resource_ec2.describe_instances()["Reservations"][0]["Instances"][0]["InstanceId"])
     except Exception as e:
-        print("exception error")
+        print(e)
+
+
+def reboot_ec2_instance():
+    try:
+        print ("Reboot EC2 instance")
+        instance_id = describe_ec2_instance()
+        resource_ec2 = boto3.client("ec2")
+        print(resource_ec2.reboot_instances(InstanceIds=[instance_id]))
+    except Exception as e:
+        print(e)
+
+
+def stop_ec2_instance():
+    try:
+        print ("Stop EC2 instance")
+        instance_id = describe_ec2_instance()
+        resource_ec2 = boto3.client("ec2")
+        print(resource_ec2.stop_instances(InstanceIds=[instance_id]))
+    except Exception as e:
+        print(e)
+
+
+def start_ec2_instance():
+    try:
+        print ("Start EC2 instance")
+        instance_id = describe_ec2_instance()
+        resource_ec2 = boto3.client("ec2")
+        print(resource_ec2.start_instances(InstanceIds=[instance_id]))
+    except Exception as e:
+        print(e)
+
+
+def terminate_ec2_instance():
+    try:
+        print ("Terminate EC2 instance")
+        instance_id = describe_ec2_instance()
+        resource_ec2 = boto3.client("ec2")
+        print(resource_ec2.terminate_instances(InstanceIds=[instance_id]))
+    except Exception as e:
+        print(e)
+
 
 #create_ec2_instance()
-describe_ec2_instance()
+#describe_ec2_instance()
+#reboot_ec2_instance()
+stop_ec2_instance()
+#start_ec2_instance()
+#terminate_ec2_instance()
